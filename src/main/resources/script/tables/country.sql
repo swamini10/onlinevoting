d478def5b536a47931ade9ef39dce08104451218
swamini10 <swaminibhandare10@gmail.com>
2025-10-23 14:41:55 +0530

    update

--- src/main/resources/script/tables/country.sql
+++ src/main/resources/script/tables/country.sql
@@ -1,7 +1,6 @@
 CREATE TABLE country (
-  id bigint NOT NULL ,
+  id bigint NOT NULL PRIMARY KEY  ,
   name VARCHAR(50) UNIQUE NOT NULL,    
   is_active bit(1) DEFAULT 1,
-  description VARCHAR(255) ,
-  id bigint PRIMARY KEY             
+  description VARCHAR(255) ,         
 );
